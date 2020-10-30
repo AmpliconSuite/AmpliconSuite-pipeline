aff9dcf17d5d20b41cf3e35a66c9060057c4b978
Jens Luebeck <jluebeck@eng.ucsd.edu>
2020-10-30 11:32:27 -0700

    bugfix to NCM install

--- docker/run_paa_script.sh
+++ docker/run_paa_script.sh
@@ -5,7 +5,7 @@ AA_SRC=/home/programs/AmpliconArchitect-master/src
 export AA_SRC
 MOSEKLM_LICENSE_FILE=/home/programs/mosek/8/licenses
 export MOSEKLM_LICENSE_FILE
-NCM_HOME=/home/programs/NGSCheckMate
+NCM_HOME=/home/programs/NGSCheckMate-master/
 export NCM_HOME
 
 ls /home > /home/output/docker_home_manifest.log
