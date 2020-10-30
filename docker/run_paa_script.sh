abc8e02d8a05296f8b0022f1ebb0a8114eabaf4f
Jens Luebeck <jluebeck@eng.ucsd.edu>
2020-10-30 10:05:58 -0700

    install NGSCheckMate into docker image

--- docker/run_paa_script.sh
+++ docker/run_paa_script.sh
@@ -5,8 +5,15 @@ AA_SRC=/home/programs/AmpliconArchitect-master/src
 export AA_SRC
 MOSEKLM_LICENSE_FILE=/home/programs/mosek/8/licenses
 export MOSEKLM_LICENSE_FILE
+NCM_HOME=/home/programs/NGSCheckMate
+export NCM_HOME
 
 ls /home > /home/output/docker_home_manifest.log
+
+#works for py2 and py3, check if NCM works
+python $NCM_HOME/ncm.py -h >> /home/output/docker_home_manifest.log
+
+# works for py2 and py3
 python programs/PrepareAA-master/PrepareAA.py $argstring &> /home/output/PAA_stdout.log
 
 #python programs/AmpliconArchitect-master/src/AmpliconArchitect.py $argstring
