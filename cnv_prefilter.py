333068d577183c0fb827a2c3829279d6b847c833
Jens Luebeck <jluebeck@eng.ucsd.edu>
2022-06-20 14:47:05 -0700

    add cn prefilter and update docker
    bugfix to prefilter

--- cnv_prefilter.py
+++ cnv_prefilter.py
@@ -41,7 +41,7 @@ def prefilter_bed(bedfile, centromere_dict, chr_sizes, cngain, outdir):
             fields = line.rstrip().rsplit("\t")
             c, s, e = fields[0], int(fields[1]), int(fields[2]) + 1
             cn = float(fields[-1])
-            a = region_ivald[c][(s + e)//2]
+            a = region_ivald[c][(s + e)//2][0]
             arm2cns[a].append((c, s, e, cn))
 
     filt_entries = []
