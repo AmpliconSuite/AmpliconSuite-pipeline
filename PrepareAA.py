9071e54460382b850d66ce1119fe3b4c66b83b4c
solo7773 <zhnanx@gmail.com>
2023-04-28 10:02:32 +0800

    Update PrepareAA.py

--- PrepareAA.py
+++ PrepareAA.py
@@ -587,8 +587,8 @@ if __name__ == '__main__':
     # set samtools for use, 20230428
     if not args.samtools_path.endswith("/samtools"):
         if args.samtools_path and not args.samtools_path.endswith("/"):
-            args.samtools_path+="/"
-        samtools_path+="samtools"
+            args.samtools_path += "/"
+        samtools_path += "samtools"
         samtools = samtools_path
 
     # Make and clear necessary directories.
